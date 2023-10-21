d53fffb89940a79c6f089651c8d3ce636ff34893
Pedro Echavarria <pechavarria62@gmail.com>
2023-10-21 08:20:02 -0500

    this is a Law firm websites

---
+++ services.php
@@ -0,0 +1,8 @@
+<!-- ======= Head ======= -->
+<?php include('components/Head.php')?>
+<!-- ======= Header ======= -->
+<?php include('components/Header.php')?>
+<!-- ======= Footer ======= -->
+<?php include('components/Footer.php')?>
+<!-- Vendor JS Files -->
+<?php include('components/Jsfiles.php')?>

b3a5d2cef635a4fb0197bc1404fc649e4ef05787
Pedro Echavarria <pechavarria62@gmail.com>
2023-10-21 20:05:35 -0500

    styling the  rating system

--- Ratings.php
+++ Ratings.php
@@ -1,17 +1,15 @@
 <!-- ======= Head ======= -->
 <?php include('components/Head.php')?>
-<!-- ======= Header ======= -->
-<?php include('components/Header.php')?>
-    <div class="container ">
-        <h1><span>Deja un Comentario y Calificación</span></h1>
+    <br><br>
+    <div class=" align-items-center justify-content-center" id="header" data-aos="fade-up"  >
+        <h1 class="active" ><span>Deja un Comentario y Calificación</span></h1>
         <form action="functions.php" method="post">
             <textarea name="comment" rows="4" cols="50" maxlength="150" required></textarea><br>
             <label for="rating">Calificación (1 a 5 estrellas):</label>
             <input type="number" name="rating" min="1" max="5" required><br>
-            <input type="submit" value="Submit">
+            <input class="get-started-btn" type="submit" value="Submit">
         </form>
-    </div>
-<!-- ======= Footer ======= -->
-<?php include('components/Footer.php')?>
+    </div><br>
+
 <!-- Vendor JS Files -->
 <?php include('components/Jsfiles.php')?>

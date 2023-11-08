b35c47a7364b2dc88e228fabe88b95de0fb35600
Pedro Echavarria <pechavarria62@gmail.com>
2023-11-08 07:57:04 -0600

    initial commit

--- index.php
+++ index.php
@@ -76,7 +76,9 @@
             </div>
         </section><!-- End About Section -->
 
-        <!-- ======= Services Section ======= -->
+        <!-- ======= Services Section ======= 
+            Derecho de lamilia
+        -->
         <section id="services" class="services">
             <div class="container" data-aos="fade-up">
 
@@ -88,17 +90,17 @@
                 <div class="row">
                 <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
                     <div class="icon-box">
-                    <div class="icon"><i class="bx bxl-dribbble"></i></div>
-                    <h4><a href="">Lorem Ipsum</a></h4>
-                    <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+                        <div class="icon"><i class="fa-solid fa-people-roof"></i></div>
+                        <h4><a href="">Derecho De Familia</a></h4>
+                        <p>Custodias ∙ Manutención ∙ Declaraciones Tardias ∙ Uniones Libres ∙ Correciones De Actas ∙ Adopciones ∙ Tutorias</p>
                     </div>
                 </div>
 
                 <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
                     <div class="icon-box">
                     <div class="icon"><i class="bx bx-file"></i></div>
-                    <h4><a href="">Sed ut perspiciatis</a></h4>
-                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+                    <h4><a href="">Asuntos Legales</a></h4>
+                    <p>Traducciones ∙ Visa Estado Unidence ∙ Recidencia Dominicana ∙ Divorcio ∙ Traspaso De Vehiculos</p>
                     </div>
                 </div>
 
